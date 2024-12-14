ce9aa97434095139ff6b58d561bc3cc3dee2ad21
Mike AUBENAS <mike.aubenas@gmail.com>
2024-12-14 20:58:44 +0100

    feat(grid): class + constructor + print grid + locate missing

---
+++ src/core/grid.rs
@@ -0,0 +1,98 @@
+
+////////////////////////////////////////
+
+use std::u8;
+
+const LENGTH_DIMENSION: u8 = 9;
+
+////////////////////
+
+#[derive(Debug)]
+enum BoxTypes {
+    Given,
+    ToBeSolved,
+    Hypothesis
+}
+
+type GridValues = Vec<Vec<u8>>;
+type GridTypes = Vec<Vec<BoxTypes>>;
+
+#[derive(Debug)]
+pub struct Grid {
+    values: GridValues,
+    types: GridTypes
+}
+
+////////////////////
+
+impl Grid {
+    pub fn new(values: GridValues) -> Self {
+        let mut types: GridTypes = vec![];
+
+        for (row_index, row) in values.iter().enumerate() {
+            if row.len() > LENGTH_DIMENSION.into() {
+                panic!("Row index {} larger than {}", row_index, LENGTH_DIMENSION)
+            }
+
+            let mut line = vec![];
+
+            for (column_index, value) in row.iter().enumerate() {
+                if row.len() > LENGTH_DIMENSION.into() {
+                    panic!("Column index {} larger than {}", column_index, LENGTH_DIMENSION)
+                }
+
+                if value > &LENGTH_DIMENSION.into() {
+                    panic!("Value '{}' out of bound at position {};{}", value, row_index, column_index);
+                }
+
+                line.push(if *value > 0 { BoxTypes::Given } else { BoxTypes::ToBeSolved } );
+            }
+
+            types.push(line);
+        }
+
+        return Grid { values, types }
+    }
+
+    //////////
+
+    fn is_line_valid(&self, line_index: u8) -> bool {
+        self.handle_index_out_of_bound(line_index);
+
+        unimplemented!()
+    }
+
+    fn is_column_valid(&self, column_index: u8) -> bool {
+        unimplemented!()
+    }
+
+    fn is_region_valid(&self, region_index: u8) -> bool {
+        unimplemented!()
+    }
+
+    pub fn locate_missing_box(&self) -> Vec<Vec<u8>> {
+        let mut locations = vec![];
+
+        for (row_index, row) in self.values.iter().enumerate() {
+            for (column_index, value) in row.iter().enumerate() {
+                if *value == 0 {
+                    locations.push(vec![row_index as u8, column_index as u8]);
+                }
+            }
+        }
+
+        return locations;
+    }
+
+    pub fn print_grid(&self, grid: Option<GridValues>) {
+        grid.unwrap_or(self.values.clone()).iter().for_each(|line| {
+            println!("{:#?}", line)
+        })
+    }
+
+    fn handle_index_out_of_bound(&self, index: u8) {
+       if index > 9 {
+           panic!("Index '{index}' out of bound");
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+mod core;
+use crate::core::grid::Grid;
+
+////////////////////////////////////////
+
 fn main() {
-    println!("Hello, world!");
+    let test_grid = Grid::new(vec!
+        [
+            vec![1, 2, 3, 4, 5, 6, 7, 8, 0],
+            vec![2, 3, 4, 5, 6, 7, 8, 9, 0],
+            vec![3, 4, 5, 6, 7, 8, 9, 1, 0],
+            vec![4, 5, 6, 7, 8, 9, 1, 2, 0],
+            vec![5, 6, 7, 8, 9, 1, 2, 3, 0],
+            vec![6, 7, 8, 9, 1, 2, 3, 4, 0],
+            vec![7, 8, 9, 1, 2, 3, 4, 5, 0],
+            vec![8, 9, 1, 2, 3, 4, 5, 6, 0],
+            vec![9, 1, 2, 3, 4, 5, 6, 7, 0],
+        ]
+    );
+
+    let missing_boxes = test_grid.locate_missing_box();
+    test_grid.print_grid(Some(missing_boxes))
 }
