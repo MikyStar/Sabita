3bdd32c450f6440b615749931a13dfcb4af0463a
Mike AUBENAS <mike.aubenas@gmail.com>
2025-01-02 20:56:12 +0100

    feat(gen): adding one more line before solve

--- src/core/generator.rs
+++ src/core/generator.rs
@@ -5,7 +5,7 @@ use super::solver::solve;
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 
-use std::{fmt, usize};
+use std::fmt;
 
 ////////////////////////////////////////
 
@@ -24,10 +24,15 @@ impl fmt::Display for GeneratingSudokuError {
 pub fn generate() -> Result<GridValues, GeneratingSudokuError> {
     let mut matrix: GridValues = vec![vec![0; 9]; 9];
 
-    // Creating a matrix with random first and last line
+    // Creating a matrix with random first, mid and last line
+    // This prevents to check for line and region
 
     let mut first_line = init_values();
     first_line.shuffle(&mut thread_rng());
+
+    let mid_line_index = 3;
+    let mut mid_line = init_values();
+    mid_line.shuffle(&mut thread_rng());
 
     let mut last_line = init_values();
     last_line.shuffle(&mut thread_rng());
@@ -37,11 +42,20 @@ pub fn generate() -> Result<GridValues, GeneratingSudokuError> {
 
         // Making last line compliant
         while first_line[index] == last_line[index] {
-            let to_move = last_line.remove(i.into());
-            last_line.push(to_move);
+            let value_to_move = last_line[index];
+            last_line.remove(i.into());
+            last_line.push(value_to_move);
+        }
+
+        // Making mid line compliant
+        while (first_line[index] == mid_line[index]) | (last_line[index] == mid_line[index]) {
+            let value_to_move = mid_line[index];
+            mid_line.remove(i.into());
+            mid_line.push(value_to_move);
         }
 
         matrix[0][index] = first_line[index];
+        matrix[mid_line_index][index] = mid_line[index];
         matrix[(LENGTH_DIMENSION - 1) as usize][index] = last_line[index];
     }
 
@@ -50,13 +64,15 @@ pub fn generate() -> Result<GridValues, GeneratingSudokuError> {
     let mut missing_locations = vec![];
 
     for line_index in 1..(LENGTH_DIMENSION - 1) {
-        for col_index in 0..LENGTH_DIMENSION {
-            let loc = BoxLocation {
-                line: line_index,
-                column: col_index,
-                region: location_to_region(&line_index, &col_index).unwrap(),
-            };
-            missing_locations.push(loc);
+        if (line_index as usize) != mid_line_index {
+            for col_index in 0..LENGTH_DIMENSION {
+                let loc = BoxLocation {
+                    line: line_index,
+                    column: col_index,
+                    region: location_to_region(&line_index, &col_index).unwrap(),
+                };
+                missing_locations.push(loc);
+            }
         }
     }
 
